1be801293b8de877991921df03251165c7b222e6
jonas nielsen <jonas18@student.sdu.dk>
2020-04-02 13:41:10 +0200

    added form for plan batch and css file

---
+++ brewsoft/mvc/app/views/Management/planbatch.php
@@ -0,0 +1,20 @@
+<body>
+    <br>
+    Please enter the correct values!
+    </br>
+    <form method="POST" action="indsæt sti her">
+        <label for="productid"> Product id: [0 1 2 3 4] </label><br>
+        <input type="text" id="productid" name="productid"><br>
+        
+        <label for="productamount"> Amount: </label><br>
+        <input type="text" id="productamount" name="productamount"><br>
+        
+        <label for="deadline"> Deadline: </label><br>
+        <input type="date" id="deadline" name="deadline"><br>
+    
+        <label for="speed"> Speed: </label><br>
+        <input type="text" id="speed" name="speed"><br>
+        
+        <input type="submit" value="Create"/>
+    </form>
+</body>
