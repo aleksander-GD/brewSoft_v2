62ffaf376185fca1de5c3b9ad832fe6935c9caf8
Jacob <Jacso18@student.sdu.dk>
2020-04-07 14:39:02 +0200

    correction

--- brewsoft/mvc/app/controllers/ManagerController.php
+++ brewsoft/mvc/app/controllers/ManagerController.php
@@ -21,27 +21,26 @@ class ManagerController extends Controller
 		//$this->view('home/batchqueue', $viewbag);
 	}
 
-	public function editBatch($identifier, $id)
+	public function editBatch($id)
 	{
-		if (strcasecmp($identifier, 'productionlistid') == 0) {
-			$batch = $this->model('Productionlist')->getQueuedBatchFromListID($id);
-				//the selected batch is sent to the view.
-			$viewbag['batch'] = $batch;
+		$batch = $this->model('Productionlist')->getQueuedBatchFromListID($id);
+			//the selected batch is sent to the view.
+		$viewbag['batch'] = $batch;
 
 				//redirect to editbatch page
-			//$this->view('home/editbatch', $viewbag);
+		//$this->view('home/editbatch', $viewbag);
 			
-				//Data that is sent via post from the view, to update the batch
-			if (isset($_POST['edit'])) {
-				$productID = filter_input(INPUT_POST, "productID", FILTER_SANITIZE_STRING);
-				$productAmount = filter_input(INPUT_POST, "productAmount", FILTER_SANITIZE_STRING);
-				$deadline = filter_input(INPUT_POST, "deadline", FILTER_SANITIZE_STRING);
-				$speed = filter_input(INPUT_POST, "speed", FILTER_SANITIZE_STRING);
+			//Data that is sent via post from the view, to update the batch
+		if (isset($_POST['edit'])) {
+			$productID = filter_input(INPUT_POST, "productID", FILTER_SANITIZE_STRING);
+			$productAmount = filter_input(INPUT_POST, "productAmount", FILTER_SANITIZE_STRING);
+			$deadline = filter_input(INPUT_POST, "deadline", FILTER_SANITIZE_STRING);
+			$speed = filter_input(INPUT_POST, "speed", FILTER_SANITIZE_STRING);
 
-				$this->model('Productionlist')->editQueuedBatch($productID, $productAmount, $deadline, $speed, $id);
+			$this->model('Productionlist')->editQueuedBatch($productID, $productAmount, $deadline, $speed, $id);
 
-					//redirect to batchqueue 
-				//$this->view('home/batchqueue', $viewbag);
+				//redirect to batchqueue 
+			//$this->view('home/batchqueue', $viewbag);
+
 		}
 	}
-}
