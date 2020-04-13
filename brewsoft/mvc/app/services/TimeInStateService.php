76a680b11ed9c0c65c1a7b320f3bcd481e4f3c24
Jacob <Jacso18@student.sdu.dk>
2020-04-13 16:06:37 +0200

    Service funktioner

--- brewsoft/mvc/app/services/TimeInStateService.php
+++ brewsoft/mvc/app/services/TimeInStateService.php
@@ -1,19 +1,33 @@
 <?php
 
-class TimeInStateService {
+class TimeInStateService
+{
 
     private $timeinstate;
-    public function __construct(){
+
+    public function __construct()
+    {
         $this->timeinstate = new TimeInState;
     }
-//$times = $this->model('TimeInState')->getTimeInStates($productionlistID);
-print_r($times);
-echo sizeof($times);
-$strStart = $times[0]['starttimeinstate'];
-$strEnd = $times[1]['starttimeinstate'];
-$dteStart = new DateTime($strStart);
-$dteEnd   = new DateTime($strEnd);
-$dteDiff  = $dteStart->diff($dteEnd);
-print $dteDiff->format("%H:%I:%S");
 
+    // Lav et array med tiden og staten, gentagelser af states tages der ikke højde for
+    public function getAllTimeInStates($productionlistid)
+    {
+        $times = $this->timeinstate->getTimeInStates($productionlistid);
+        print_r($times);
+        echo sizeof($times);
+        $strStart = $times[0]['starttimeinstate'];
+        $strEnd = $times[1]['starttimeinstate'];
+        $dteStart = new DateTime($strStart);
+        $dteEnd   = new DateTime($strEnd);
+        $dteDiff  = $dteStart->diff($dteEnd);
+        print $dteDiff->format("%H:%I:%S");
+    }
+
+    // En funktion som lægger gentagende states sammen
+    public function getSortedTimeInStates($AllTimeInStatesList){
+
+    }
+
+    //evt funktion som fjerne dem som vi ikke vil have med?
 }
