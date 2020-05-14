e4c8f945c65c3f60d6b7b4a417c9d49c3b6c9e87
AleksanderGD <agdbusiness@hotmail.com>
2020-05-14 12:11:48 +0200

    update on menu
    Added admin, brewworker and manager navigation bar.

--- brewsoft/mvc/app/views/manager/showOeeForBatch.php
+++ brewsoft/mvc/app/views/manager/showOeeForBatch.php
@@ -7,8 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../css/oeeforbatch.css">
     <script type='text/javascript' src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-    
-
 </head>
 
 <body>
