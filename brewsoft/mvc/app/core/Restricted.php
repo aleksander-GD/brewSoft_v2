90226cb6b9bfbe09ddc4e17879089e6b11f22d27
jonas nielsen <jonas18@student.sdu.dk>
2020-05-15 16:00:26 +0200

    Fixed router with små og store bogstaver

--- brewsoft/mvc/app/core/Restricted.php
+++ brewsoft/mvc/app/core/Restricted.php
@@ -13,7 +13,7 @@ function restricted($controller, $method)
 
 	if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true) {
 		return false;
-	} else if (isset($controller) && in_array($method,array_map('strtolower', $restricted_urls[$controller]))) {
+	} else if (isset($controller) && in_array(strtolower($method), array_map('strtolower', $restricted_urls[$controller]))) {
 		return true;
 	} else {
 		return false;
