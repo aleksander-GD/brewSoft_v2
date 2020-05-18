47bcdcf200aaad3f79ef7034f008c794534e51fa
Jacob <Jacso18@student.sdu.dk>
2020-05-18 11:38:29 +0200

    Init

---
+++ brewsoft/mvc/app/controllers/DatabasePerformanceController.php
@@ -0,0 +1,15 @@
+<?php
+
+class DatabasePerformanceController extends Controller
+{
+    public function index()
+    {
+        
+    }
+
+    public function testDatabaseConnections(){
+        
+    }
+
+
+}
---
+++ brewsoft/mvc/app/models/Connectiontest.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/brewsoft/mvc/app/core/ConnectionTestDatabase.php';
+
+class Finalbatchinformation extends ConnectionTestDatabase
+{
+
+}
+?>
